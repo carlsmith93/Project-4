35bc3631476cf1c70e3a179163efdd95b91996b8
carlsmith93 <cesmith@fullsail.edu>
2014-11-20 14:50:10 -0400

    Added Function to pick lowest number from array higher than the user input

--- rename-me.js
+++ rename-me.js
@@ -1,7 +1,29 @@
-var integer = function(numberstring){  //convert string to number datatype.  MAKEUP: Project 2 New Technical/Functions
+//convert string to number datatype.
+
+var integer = function(numberstring){  //  MAKEUP: Project 2 New Technical/Functions
 
 parseInt(numberstring)
 return numberstring
 };
 var inputnumber = integer(prompt("enter any number here", ""))
 console.log(inputnumber)
+
+
+//Find the smallest value in an array than is greater than a given number.
+
+var smallestvalue = function(inputvalue){
+var numarray = [1,12,18]	
+
+parseInt(smallestvalue);
+if (smallestvalue < 1){
+	console.log(numarray[0])
+}
+else if ( 12 > smallestvalue){
+	console.log(numarray[1])
+}
+else{
+	console.log(numarray[2])
+}
+};
+	
+smallestvalue(prompt("enter a number here", ""))
