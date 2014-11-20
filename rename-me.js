5a360ea62b83369e9a306809b506bbc36060ada6
carlsmith93 <cesmith@fullsail.edu>
2014-11-20 14:26:12 -0400

    Added Function to convert a string to a number data type

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,7 @@
-alert("JavaScript works!");
+var integer = function(numberstring){  //convert string to number datatype.  MAKEUP: Project 2 New Technical/Functions
+
+parseInt(numberstring)
+return numberstring
+};
+var inputnumber = integer(prompt("enter any number here", ""))
+console.log(inputnumber)
